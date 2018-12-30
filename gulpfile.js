c7d04e0c8a64c9dec8b3bb994d552960da63913a
arturofulldev <arturo.fulldev@gmail.com>
2018-12-29 18:05:57 -0600

    integracion de libreria oswald y fontawesome, integracion de formulario contacto, algunos estilos para formulario contacto, insercion de video, primeras pruebas de elemeto smoot scroll

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let gulp = require('gulp');
 let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
 let minifycss =require('gulp-minify-css');
 let concatCss = require('gulp-concat-css');
 
@@ -8,17 +9,19 @@ let concat = require('gulp-concat');
  
 sass.compiler = require('node-sass');
  
-gulp.task('sass', function () {
+gulp.task('bootstrap-sass', function () {
   return gulp.src('./node_modules/bootstrap/scss/**/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer())
     .pipe(concatCss("bundle.min.css"))
     .pipe(minifycss())
     .pipe(gulp.dest('libs/bootstrap'));
 });
 
-gulp.task('js', function () {
-  return gulp.src('./node_modules/bootstrap/js/dist/**/*.js')
-    .pipe(uglify())
-    .pipe(concat('bundle.min.js'))
-    .pipe(gulp.dest('libs/bootstrap'));
+gulp.task('sass', function () {
+  return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(concatCss("mine.min.css"))
+    .pipe(minifycss())
+    .pipe(gulp.dest('assets/css'));
 });
