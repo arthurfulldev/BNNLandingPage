b4aac295a7e4a1825bb214d2630c94836f730678
arturofulldev <arturo.fulldev@gmail.com>
2019-01-05 19:41:01 -0600

    Actualizacion de estilos rotos de responsive design

--- assets/js/smoot.js
+++ assets/js/smoot.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('body').scrollspy({ target: '#navbar-example2' });
+    $('body').scrollspy({ target: '#bnn-navbar' });
     $("a").on('click', function (event) {
         $('.nav-item').removeClass('active');
         $(this).parent('li').addClass('active');
@@ -7,7 +7,7 @@ $(document).ready(function () {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
+                scrollTop: (($(hash).offset().top) + 15)
             }, 800, function () {
                 window.location.hash = hash;
             });
